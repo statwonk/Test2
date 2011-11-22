f13f0d95a24303a1ab0b64688ffaed9327b86d20
Christopher Peters <cpeter9@gmail.com>
2011-11-21 20:50:36 -0600

    Update 8:50pm

--- Problems1and2 v2.R
+++ Problems1and2 v2.R
@@ -25,9 +25,18 @@ lines(log(tmp.time),qnorm(tmp.prob))
 
 # Make some data for 2a)
 tmp.time <- log.seq(1,20,length=100)
-log.tmp.time <- tmp.time
-tmp.prob <- psev(tmp.time, location = 10, scale = 1)
+lon.tmp.time <- log(tmp.time)
+tmp.prob <- psev(log.tmp.time, location = log(10), scale = 1)
 
 # Plot 2a)
 probpaper(x.range=c(1,20), y.range=c(0.021,.989),  distribution="weibull", grids = TRUE,ylab = "Probability", xlab = "Time", my.title="The title")
-lines(tmp.time, qsev(tmp.prob, shape = 10, scale = 1))
+lines(log(tmp.time), qsev(tmp.prob))
+
+# Make some data for 2b)
+tmp.time <- log.seq(1,20,length=100)
+lon.tmp.time <- log(tmp.time)
+tmp.prob <- psev(log.tmp.time, location = log(10), scale = 2)
+
+# Plot 2b)
+probpaper(x.range=c(1,20), y.range=c(0.021,.989),  distribution="weibull", grids = TRUE,ylab = "Probability", xlab = "Time", my.title="The title")
+lines(log(tmp.time), qsev(tmp.prob))
