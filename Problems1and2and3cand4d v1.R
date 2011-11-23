7a487e405b1376d755bc00729bbf1ea67d3ddc4d
Christopher Peters <cpeter9@gmail.com>
2011-11-23 00:11:04 -0600

    final test2 update

--- Problems1and2and3cand4d v1.R
+++ Problems1and2and3cand4d v1.R
@@ -51,7 +51,38 @@ names(problem4c) <- c("Time", "Status")
 
 simple.contour(problem4c, "L")
 
+# First weibull
+tmp.time <- log.seq(1,100,length=100)
+lon.tmp.time <- log(tmp.time)
+tmp.prob <- psev(log.tmp.time, location = log(30), scale = 1)
+probpaper(x.range=c(1,100), y.range=c(0.021,.989),  distribution="weibull", grids = TRUE,ylab = "Probability", xlab = "Time", my.title="The title")
+lines(log(tmp.time), qsev(tmp.prob))
+
+# Second weibull
+tmp.time <- log.seq(1,100,length=100)
+lon.tmp.time <- log(tmp.time)
+tmp.prob <- psev(log.tmp.time, location = log(40), scale = 2)
+probpaper(x.range=c(1,100), y.range=c(0.021,.989),  distribution="weibull", grids = TRUE,ylab = "Probability", xlab = "Time", my.title="The title")
+lines(log(tmp.time), qsev(tmp.prob))
+
+
 # Generating random Weibull numbers
 
 first_weibull <- rweibull(1:100, 30, 1)
 second_weibull <- rweibull(1:100, 40, 2)
+first_weibull_cdf <- pweibull(first_weibull)
+
+weibulls2 <- as.data.frame(first_weibull)
+weibulls2$second_weibull <- second_weibull
+
+write.csv(weibulls2, file = "C:\\Users\\Chris\\School\\EXST_Reliability_and_Survival_Analysis\\weibulls_prob4.csv")
+
+hazard_num <- dweibull(first_weibull, 30, 1)*dweibull(second_weibull, 40, 2)
+
+hazard_denom <- (-pweibull(first_weibull, 30, 1, lower = FALSE))*(-pweibull(second_weibull, 40, 2, lower = FALSE))
+hazard <- hazard_num/hazard_denom
+
+probpaper(x.range=c(1,100), y.range=c(0.021,.989),  distribution="weibull", grids = TRUE,ylab = "Probability", xlab = "Time", my.title="The title")
+lines(log(tmp.time), qweibull(hazard))
+
+
